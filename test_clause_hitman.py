dbedb22510079c59029e0ce6ff75b56e82a7ae6c
GuillaumeHERMOSO <126562085+GuillaumeHERMOSO@users.noreply.github.com>
2023-05-13 18:50:13 +0200

    clause verite_sur_le_monde + test gophersat

--- test_clause_hitman.py
+++ test_clause_hitman.py
@@ -26,35 +26,16 @@ Model = List[Literal]
 #00_V
 #00_I_N
 
-#{"00" = ["00_V","00_M","00_C","00_D","00_T","00_I_N","00_I_S","00_I_E","00_I_O","00_G_N","00_G_S","00_G_E","00_G_O""]}
-def creer_dico_var(m,n):
-    dict_cases = {}
-    for i in range(1,m+1):
-        for j in range(1,n+1):
-            dict_cases["%d%d"%(i,j)] = ["%d%d_V"%(i,j),"%d%d_M"%(i,j),"%d%d_C"%(i,j),"%d%d_D"%(i,j),"%d%d_T"%(i,j),"%d%d_I_N"%(i,j),"%d%d_I_S"%(i,j),"%d%d_I_E"%(i,j),"%d%d_I_O"%(i,j),"%d%d_G_N"%(i,j),"%d%d_G_S"%(i,j),"%d%d_G_E"%(i,j),"%d%d_G_O"%(i,j)]
-    return dict_cases
-#print(creer_dico_var(m=2,n=2))
 
 # ["00_V","00_M","00_C",...,"00_G_O","01_V",...,"58_G_O"]
 def creer_list_var(m,n):
     list_cases = []
     for i in range(0,m):
         for j in range(0,n):
-            for reste in ["V","M","C","D","T","I","I_N","I_S","I_E","I_O","G","G_N","G_S","G_E","G_O"]:
+            for reste in ["V","M","C","D","T","I","I_N","I_S","I_E","I_O","G","G_N","G_S","G_E","G_O","P"]:
                 list_cases.append("%d%d_%s"%(i,j,reste))
     return list_cases
-#print(creer_list_var(m=2,n=2))
 
-
-def creer_dictionnaire_cases_par_dico(dict_variable):
-    # Création du dictionnaire une variable = un chiffre:
-    compteur = 1
-    dict_cases = {}
-    for position in dict_variable.keys():
-        for i in dict_variable[position]:
-            dict_cases[i] = compteur
-            compteur += 1
-    return dict_cases
 
 def creer_dictionnaire_cases_par_list(list_variable):
     # Création du dictionnaire une variable = un chiffre:
@@ -64,18 +45,6 @@ def creer_dictionnaire_cases_par_list(list_variable):
         dict_cases[i] = compteur
         compteur += 1
     return dict_cases
-#print(creer_dictionnaire_cases_par_dico(creer_dico_var(m=2,n=2)))
-#print(creer_dictionnaire_cases_par_list(creer_list_var(m=2,n=2)))
-
-def at_least_one(v: List[PropositionnalVariable]) -> Clause:
-    return v
-
-def unique(variables: List[PropositionnalVariable]) -> ClauseBase:
-    r: ClauseBase = [at_least_one(variables)]
-    for a, b in combinations(variables, 2):
-        r.append([-a, -b])
-    print(r)
-    return r
 
 #### fonctions fournies
 
@@ -104,74 +73,6 @@ def exec_gophersat(
 
 
 #clause verité sur le monde
-def clause_il_y_a_K_garde(m,n,k):
-    #variables = ["00","01", .."08","10","11",..."18",..."57","58"]
-    variables = []
-    r: ClauseBase = []
-    for i in range(0,m):
-        for j in range(0,n):
-            variables.append("%d%d_G"%(i,j))
-    #creation d'un tableau de taille k pour recuperer les variable des combinaisons
-
-    for tab in combinations(variables, k+1):
-        r.append(["-"+ x for x in tab])
-    for tab in combinations(variables, len(variables)+1-k):
-        r.append([x for x in tab])
-    return r
-
-def clause_il_y_a_K_invite(m,n,k):
-        #variables = ["00","01", .."08","10","11",..."18",..."57","58"]
-    variables = []
-    r: ClauseBase = []
-    for i in range(0,m):
-        for j in range(0,n):
-            variables.append("%d%d_I"%(i,j))
-    #creation d'un tableau de taille k pour recuperer les variable des combinaisons
-
-    for tab in combinations(variables, k+1):
-        r.append(["-"+ x for x in tab])
-    for tab in combinations(variables, len(variables)+1-k):
-        r.append([x for x in tab])
-    return r
-
-def clause_une_cible(m,n):
-    variables = []
-    r: ClauseBase = []
-    for i in range(0,m):
-        for j in range(0,n):
-            variables.append("%d%d_T"%(i,j))
-    #creation d'un tableau de taille k pour recuperer les variable des combinaisons
-    for tab in combinations(variables, 2):
-        r.append(["-"+ x for x in tab])
-    for tab in combinations(variables, len(variables)+1-2):
-        r.append([x for x in tab])
-    return r
-
-def clause_une_corde(m,n):
-    variables = []
-    r: ClauseBase = []
-    for i in range(0,m):
-        for j in range(0,n):
-            variables.append("%d%d_C"%(i,j))
-    #creation d'un tableau de taille k pour recuperer les variable des combinaisons
-    for tab in combinations(variables, 2):
-        r.append(["-"+ x for x in tab])
-    for tab in combinations(variables, len(variables)+1-2):
-        r.append([x for x in tab])
-    return r
-
-def clause_un_deguisement(m,n):
-    variables = []
-    r: ClauseBase = []
-    for i in range(0,m):
-        for j in range(0,n):
-            variables.append("%d%d_D"%(i,j))
-    #creation d'un tableau de taille k pour recuperer les variable des combinaisons
-    for tab in combinations(variables, 2):
-        r.append(["-"+ x for x in tab])
-    for tab in combinations(variables, len(variables)+1-2):
-        r.append([x for x in tab])
-    return r
 
 def recup_var_G(list_cases,dict_var_to_num):
     var = []
@@ -191,29 +92,7 @@ def exactly_k(k: int, variables: List[PropositionnalVariable]) -> ClauseBase:
     return r
 
 def main():
-    m=5
-    n=8
-    l_var = creer_list_var(m,n)
-    dict_var_to_num = creer_dictionnaire_cases_par_list(l_var)
-
-    print(recup_var_G(l_var,dict_var_to_num))
-    for i in recup_var_G(l_var,dict_var_to_num):
-        key = [k  for (k, val) in dict_var_to_num.items() if val == i]
-        print(key)
-
-    clause_2_garde = exactly_k(2,recup_var_G(l_var,dict_var_to_num))
-    clause_2_garde_v1 = clause_il_y_a_K_garde(m,n,2)
-    print(len(clause_2_garde))
-    print(len(clause_2_garde_v1))
     
-    print(clause_2_garde_v1[0])
-    #[['-00_G', '-01_G', '-02_G'], ['-00_G', '-01_G', '-03_G']]
-    print(dict_var_to_num["00_G"],dict_var_to_num["01_G"],dict_var_to_num["02_G"],"\n")
-    print(clause_2_garde[0])
-
-    clause_1_garde = exactly_k(1,recup_var_G(l_var,dict_var_to_num))
-    # 2 parmis 40 780 + 1
-    print(len(clause_1_garde))
 
     
     pass
---
+++ verite_sur_le_monde.py
@@ -0,0 +1,136 @@
+from pprint import pprint
+from time import sleep
+from typing import List, Tuple
+import subprocess
+from itertools import combinations
+
+from test_clause_hitman import *
+
+Grid = List[List[int]]
+PropositionnalVariable = int
+Literal = int
+Clause = List[Literal]
+ClauseBase = List[Clause]
+Model = List[Literal]
+
+
+def recup_var_G(list_cases,dict_var_to_num):
+    var = []
+    for i in list_cases:
+        #on recupere les variables du type 00_G mais pas 00_G_N
+        if i[-1] == "G" :
+            var.append(dict_var_to_num[i])
+
+    return var
+def gen_var_lettre(m,n,dict_var_to_num,lettre : str):
+    var = []
+    for i in range(m):
+        for j in range(n):
+            var.append(dict_var_to_num[f"{i}{j}_{lettre}"])
+    return var
+
+def gen_var_par_case(m,n,list_cases,dict_var_to_num, sauf :list[str] =["P","G","I"]):
+    # {"00": ["00_V","00_M","00_C",...,"00_G_O"]
+    #  "01": ["01_V",...,"01_G_O"]
+    #  ...}
+    dico_var_par_case = {}
+    for i in range(m):
+        for j in range(n):
+            dico_var_par_case[f"{i}{j}"] = []
+    
+    for k in list_cases:
+        if k[-1] not in sauf:
+            dico_var_par_case[k[:2]].append(dict_var_to_num[k])
+    return dico_var_par_case 
+
+#00_P<-> 00_Gou 00_i  : (-00_P ou 00_G ou 00_I ) et (-00_G ou 00_P) et (-00_I  ou 00_P)
+def gen_clause_P_equi_G_ou_I(m,n,dict_var_to_num):
+    r: ClauseBase = []
+    #on genere les variables
+    var_P = gen_var_lettre(m,n,dict_var_to_num,"P")
+    var_G = gen_var_lettre(m,n,dict_var_to_num,"G")
+    var_I = gen_var_lettre(m,n,dict_var_to_num,"I")
+    for i in range(len(var_P)):
+        r.append([-var_P[i],var_G[i],var_I[i]])
+        r.append([-var_G[i],var_P[i]])
+        r.append([-var_I[i],var_P[i]])
+    return r
+
+def gen_clause_lettre_equi_lettre_orientation(m,n,dict_var_to_num,lettre : str):
+    r: ClauseBase = []
+    #on genere les variables
+    var_l = gen_var_lettre(m,n,dict_var_to_num,f"{lettre}")
+    var_l_N = gen_var_lettre(m,n,dict_var_to_num,f"{lettre}_N")
+    var_l_S = gen_var_lettre(m,n,dict_var_to_num,f"{lettre}_S")
+    var_l_E = gen_var_lettre(m,n,dict_var_to_num,f"{lettre}_E")
+    var_l_O = gen_var_lettre(m,n,dict_var_to_num,f"{lettre}_O")
+    for i in range(len(var_l)):
+        r.append([-var_l[i],var_l_N[i],var_l_S[i],var_l_E[i],var_l_O[i]])
+        r.append([-var_l_N[i],var_l[i]])
+        r.append([-var_l_S[i],var_l[i]])
+        r.append([-var_l_E[i],var_l[i]])
+        r.append([-var_l_O[i],var_l[i]])
+    return r
+
+def exactly_k(k: int, variables: List[PropositionnalVariable]) -> ClauseBase:
+    r: ClauseBase = []
+    for tab in combinations(variables, k+1):
+        r.append([-x for x in tab])
+    for tab in combinations(variables, len(variables)+1-k):
+        r.append([x for x in tab])
+    return r
+
+#Écrire une fonction clauses_to_dimacs(clauses: ClauseBase, nb_vars: int) -> str qui,
+#étant donné une base de clauses et le nombre de variables à considérer, renvoie une
+#chaîne de caractères codant la base de clauses au format Dimacs.
+#>>> clauses_to_dimacs([[-1, -2], [1, 2], [1, 3], [2, 4], [-3, 4], [-4, 5]], 5)
+#'p cnf 5 6\n-1 -2 0\n1 2 0\n1 3 0\n2 4 0\n-3 4 0\n-4 5 0\n'
+
+def clauses_to_dimacs(clauses: ClauseBase, nb_vars: int) -> str:
+    r = f"p cnf {nb_vars} {len(clauses)}\n"
+    for clause in clauses:
+        for literal in clause:
+            r += f"{literal} "
+        r += "0\n"
+    return r
+
+
+def main():
+    list_var = creer_list_var(5,8)
+
+    dico_var_to_num = creer_dictionnaire_cases_par_list(list_var)
+
+    #on recupere les variables
+    var_Guarde =  gen_var_lettre(5,8,dico_var_to_num,"G")
+    var_Target =  gen_var_lettre(5,8,dico_var_to_num,"T")
+    var_Invite =  gen_var_lettre(5,8,dico_var_to_num,"I")
+    var_Corde  =  gen_var_lettre(5,8,dico_var_to_num,"C")
+    dico_par_case = gen_var_par_case(5,8,list_var,dico_var_to_num)
+
+
+    #on genere les clauses
+    V_sur_le_monde : ClauseBase = []
+    V_sur_le_monde += exactly_k(2,var_Guarde)
+    V_sur_le_monde += exactly_k(3,var_Invite)
+    V_sur_le_monde += exactly_k(1,var_Target)
+    V_sur_le_monde += exactly_k(1,var_Corde)
+    for i in dico_par_case:
+        V_sur_le_monde += exactly_k(1,dico_par_case[i])
+
+    V_sur_le_monde += gen_clause_P_equi_G_ou_I(5,8,dico_var_to_num)
+    V_sur_le_monde += gen_clause_lettre_equi_lettre_orientation(5,8,dico_var_to_num,"G")
+    V_sur_le_monde += gen_clause_lettre_equi_lettre_orientation(5,8,dico_var_to_num,"I")
+
+    print(len(list_var))
+    print(len(V_sur_le_monde))
+
+    dimac = clauses_to_dimacs(V_sur_le_monde,len(list_var))
+    #print(dimac)
+    write_dimacs_file(dimac, "./test.cnf")
+    res = exec_gophersat( "test.cnf")
+    print(res)
+    pass
+
+
+if __name__ == "__main__":
+    main()
